458159cbc1de0881fa89edd9411788405587f00a
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-10-24 13:57:25 +0900

    Update BrotherPrinterDocument.cs

--- BrotherPrinterDocument.cs
+++ BrotherPrinterDocument.cs
@@ -9,7 +9,7 @@ namespace PtouchPrintSender
 	/// <summary>
 	/// brother の b-PAC(Brother P-touch Applicable Component、COM コンポーネント)を用いて、プリンター出力を行うクラスです。
 	/// </summary>
-	sealed class BrotherPrinterDocument
+	sealed class BrotherPrinterDocument : IDisposable
 	{
 		/// <summary>
 		/// ドキュメントオブジェクト
@@ -67,6 +67,12 @@ namespace PtouchPrintSender
 			// まとめて印刷した方がパフォーマンスが向上します。
 			// this._document.EndPrint();
 			this._document.Close();
+			Console.WriteLine("[DEBUG] プリンターオブジェクトが解放されました。");
+		}
+
+		public void Dispose()
+		{
+			this.Close();
 		}
 	}
 }
