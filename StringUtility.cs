22fc7adedf548a1b2294592393ddd7709cdc0b49
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-04-29 10:44:09 +0900

    Create StringUtility.cs

---
+++ StringUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PtouchPrintSender
+{
+	internal static class StringUtility
+	{
+		public static bool IsEmptyOrNull(string s)
+		{
+			return s == null || s == "";
+		}
+	}
+}
