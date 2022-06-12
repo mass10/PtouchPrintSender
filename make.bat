5738551b3add709dda473b71691dda173d0fb20f
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-12 11:08:29 +0900

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,6 +1,4 @@
+@SET PP_MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe
 
-@DEL bin\Release\*.exe
-
-@"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" PTouchPrintSender.sln /p:configuration="Release"
-
-@DIR /S /B bin\*.exe
+@CALL cargo fmt
+@CALL cargo run %*
