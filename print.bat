a441d43f67bfcc8e80ec4c8ed938d0bd4b07af15
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-19 23:12:41 +0900

    Update print.bat

--- print.bat
+++ print.bat
@@ -1,4 +1,9 @@
-@SET PT_DRYRUN=
+@REM
+@REM ˆóü‚ğs‚¢‚Ü‚·B
+@REM
+
+@REM ==================== dry-run İ’è ====================
 @SET PT_DRYRUN=true
+@SET PT_DRYRUN=
 
 @CALL bin\Release\PtouchPrintSender.exe %*
