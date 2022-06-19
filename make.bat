645ae03039ee23ff013a24d6050a08afebf1909a
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-19 23:11:05 +0900

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,3 +1,13 @@
+@REM
+@REM ビルド用のランチャーです。
+@REM
+@REM 【使用方法】
+@REM  make を実行すると、アプリケーション本体がビルドされます。
+@REM
+@REM 【注意】
+@REM  * Rust によって記述されています。rustup が必要です。
+@REM
+
 @SET PP_MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe
 
 @CALL cargo fmt
