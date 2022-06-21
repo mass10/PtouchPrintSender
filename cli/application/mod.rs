7c13a1c42981a35a7272f0e9f192c6ddf1485279
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-21 22:40:49 +0900

    Create mod.rs

---
+++ cli/application/mod.rs
@@ -0,0 +1,60 @@
+use crate::{info, util};
+
+/// コマンドを実行します。
+fn execute_command(commands: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
+	let (command, arguments) = commands.split_first().unwrap();
+	info!("コマンドを呼び出しています... [{}]", command);
+	let exit_status = std::process::Command::new(command).args(arguments).spawn()?.wait()?;
+	if !exit_status.success() {
+		let code = exit_status.code().unwrap();
+		std::process::exit(code);
+	}
+	return Ok(());
+}
+
+/// OS のシェル内でコマンドを実行します。
+#[allow(unused)]
+fn execute_shell_command(command: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
+	let exit_status = std::process::Command::new("cmd.exe").arg("/C").args(command).spawn()?.wait()?;
+	if !exit_status.success() {
+		let code = exit_status.code().unwrap();
+		std::process::exit(code);
+	}
+	return Ok(());
+}
+
+/// ソリューションをビルドします。
+///
+/// # Arguments
+/// * `name` - ビルドするソリューションファイル(.sln)へのパス。.csproj などを指定しても構わない。
+fn build_solution(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+	let msbuild = util::getenv("PP_MSBUILD");
+	execute_command(&[&msbuild, path, r#"/p:configuration=Release"#])?;
+	return Ok(());
+}
+
+/// アプリケーションを実行します。
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+	info!("##### START BUILD #####");
+
+	// ビルドの出力ファイル
+	const OUT_PATH: &str = r#"bin\Release\PTouchPrintSender.exe"#;
+
+	// 最初のタイムスタンプ
+	let former_filetime = util::get_filetime(OUT_PATH).unwrap_or_default();
+
+	// ソリューションをビルドします。
+	build_solution("PTouchPrintSender.sln")?;
+
+	// ビルド後のタイムスタンプ
+	let current_filetime = util::get_filetime(OUT_PATH)?;
+
+	// 確認
+	if former_filetime == current_filetime {
+		info!("ファイル [{}] は更新されませんでした。[{}] >> [{}]", OUT_PATH, &former_filetime, &current_filetime);
+		return Ok(());
+	}
+	info!("ファイル [{}] をビルドしました。[{}] >> [{}]", OUT_PATH, &former_filetime, &current_filetime);
+
+	return Ok(());
+}
