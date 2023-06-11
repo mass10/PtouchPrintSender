9b0eeb5d8feb44f421aec7ca6d5434e7228e4cc9
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-06-11 10:08:02 +0900

    Update ConfigurationManager.cs

--- ConfigurationManager.cs
+++ ConfigurationManager.cs
@@ -37,14 +37,61 @@ namespace PtouchPrintSender
 			var conf = new ConfigurationManager();
 
 			// PT_DRYRUN
-			var dryrun = ParseBoolean(System.Environment.GetEnvironmentVariable("PT_DRYRUN", System.EnvironmentVariableTarget.Process));
-			conf.dryrun = dryrun;
+			var dryrun = false; //ParseBoolean(System.Environment.GetEnvironmentVariable("PT_DRYRUN", System.EnvironmentVariableTarget.Process));
+			// conf.dryrun = dryrun;
 
-			if (System.Environment.GetCommandLineArgs().Contains("--dryrun"))
+			// コマンドライン引数の読み取り
+			// * OPTION: --dryrun (bool)
+			// * OPTION: --address-file (multiple)
+
+			var addressFiles = new List<string>();
+			var currentSection = "";
+
+			foreach (var arg in System.Environment.GetCommandLineArgs().Skip(1))
 			{
-				conf.dryrun = true;
+				if (arg == "--dryrun")
+				{
+					currentSection = "dryrun";
+					conf.dryrun = true;
+				}
+				else if (arg.StartsWith("--address-file="))
+				{
+					// オプションと値が同時に指定されている
+					addressFiles.Add(arg.Substring("--address-file=".Length));
+				}
+				else if (arg == "--address-file")
+				{
+					// オプションの後に値が続く
+					currentSection = "address-file";
+				}
+				else if (arg.StartsWith("--"))
+				{
+					// その他のオプションは無い
+					currentSection = "";
+					throw new Exception("Invalid argument: [" + arg + "]");
+				}
+				else if (currentSection == "dryrun")
+				{
+					currentSection = "";
+					throw new Exception("Invalid argument: [" + arg + "]");
+				}
+				else if (currentSection == "address-file")
+				{
+					if (arg != "") addressFiles.Add(arg);
+					currentSection = "";
+				}
+				else
+				{
+					// 不明な文字列
+					throw new Exception("Invalid argument: [" + arg + "]");
+				}
 			}
-			//Console.WriteLine("PT_DRYRUN: " + conf.dryrun);
+
+			Console.WriteLine("--dryrun: " + conf.dryrun);
+			Console.WriteLine("--address-file: " + string.Join(", ", addressFiles));
+
+			throw new Exception("テスト完了");
+
 			return conf;
 		}
 	}
