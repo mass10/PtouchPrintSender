4e0b64d21d069dc06d8b33ef7214829313fd1a97
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-06-11 10:58:39 +0900

    Update Application.cs

--- Application.cs
+++ Application.cs
@@ -25,6 +25,14 @@ namespace PtouchPrintSender
 		/// <param name="conf"></param>
 		public void Run(ConfigurationManager conf)
 		{
+			foreach (var filepath in conf.addressFiles)
+			{
+				Print(filepath, conf.dryrun);
+			}
+		}
+
+		private void Print(string filepath, bool dryrun)
+		{
 			// ドキュメントオブジェクトを初期化
 			var document = new BrotherPrinterDocument();
 
@@ -35,14 +43,13 @@ namespace PtouchPrintSender
 				document.AttachDocumentTemplate("宛名.lbx");
 
 				// アドレス帳を開きます。
-				var reader = new System.IO.StreamReader("ADDRESS.tsv", System.Text.Encoding.UTF8);
+				var reader = new System.IO.StreamReader(filepath, System.Text.Encoding.UTF8); 
 				while (true)
 				{
 					var line = reader.ReadLine();
 					if (line == null)
 						// EOF
 						break;
-
 					if (line == "")
 						// 無視
 						continue;
@@ -50,36 +57,12 @@ namespace PtouchPrintSender
 						// 無視
 						continue;
 
-					var columns = line.Split('\t');
-					// 郵便番号
-					var postCode = StringUtility.At(columns, 0);
-					if (postCode == "")
+					var fields = ParseAddressLine(line);
+					if (fields == null)
+						// 無視
 						continue;
-					// 住所
-					var address = StringUtility.At(columns, 1);
-					if (address == "")
-						continue;
-					// お名前
-					var name = StringUtility.At(columns, 2);
-					// 電話番号(もしあれば)
-					var phoneNumber = StringUtility.At(columns, 3);
-					// 製品名(もしあれば)
-					var productName = StringUtility.At(columns, 4);
-					// サイズ(もしあれば)
-					var size = StringUtility.At(columns, 5);
 
-					var fields = new Dictionary<string, string>();
-					// 宛名
-					fields["AddressText"] = String.Format("{0} {1}", postCode, address);
-					// お名前
-					fields["NameText"] = StringUtility.FixTargetName(columns[2], "", productName, size);
-					// 配送会社向け連絡先
-					fields["PhoneText"] = StringUtility.FormatPhoneNumber(phoneNumber);
-
-					Console.WriteLine("[DEBUG] 住所: [{0}], 氏名: [{1}], 電話: [{2}]",
-							fields["AddressText"], fields["NameText"], fields["PhoneText"]);
-
-					if (conf.dryrun)
+					if (dryrun)
 						// 印刷しない
 						continue;
 
@@ -91,6 +74,43 @@ namespace PtouchPrintSender
 			{
 				document.Close();
 			}
+		}
+
+		/// <summary>
+		/// アドレス帳の行をパースします。
+		/// </summary>
+		private static Dictionary<string, string> ParseAddressLine(string line)
+		{
+			var columns = line.Split('\t');
+			// 郵便番号
+			var postCode = StringUtility.At(columns, 0);
+			if (postCode == "")
+				return null;
+			// 住所
+			var address = StringUtility.At(columns, 1);
+			if (address == "")
+				return null;
+			// お名前
+			var name = StringUtility.At(columns, 2);
+			// 電話番号(もしあれば)
+			var phoneNumber = StringUtility.At(columns, 3);
+			// 製品名(もしあれば)
+			var productName = StringUtility.At(columns, 4);
+			// サイズ(もしあれば)
+			var size = StringUtility.At(columns, 5);
+
+			var fields = new Dictionary<string, string>();
+			// 宛名
+			fields["AddressText"] = String.Format("{0} {1}", postCode, address);
+			// お名前
+			fields["NameText"] = StringUtility.FixTargetName(columns[2], "", productName, size);
+			// 配送会社向け連絡先
+			fields["PhoneText"] = StringUtility.FormatPhoneNumber(phoneNumber);
+
+			Console.WriteLine("[DEBUG] 住所: [{0}], 氏名: [{1}], 電話: [{2}]",
+					fields["AddressText"], fields["NameText"], fields["PhoneText"]);
+
+			return fields;
 		}
 	}
 }
