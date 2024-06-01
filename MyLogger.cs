45f85caa12167589bb2371baa2cd97ea1f1bf377
Masaru Irisawa 入澤 賢 <irisawa.masaru@gmail.com>
2024-06-01 11:35:41 +0900

    Update MyLogger.cs

--- MyLogger.cs
+++ MyLogger.cs
@@ -6,20 +6,23 @@ using System.Threading.Tasks;
 
 namespace PtouchPrintSender
 {
+    /// <summary>
+    /// ロガー
+    /// </summary>
     class MyLogger
     {
+        /// <summary>
+        /// コンストラクターは非公開です。
+        /// </summary>
         private MyLogger()
         {
 
         }
 
-        //public static void Info(string format, params object[] args)
-        //{
-        //    var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-        //    var line = string.Format(format, args);
-        //    Console.WriteLine($"{timestamp} [INFO] {line}");
-        //}
-
+        /// <summary>
+        /// ロギング(INFO)
+        /// </summary>
+        /// <param name="args"></param>
         public static void Info(params object[] args)
         {
             var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
@@ -27,13 +30,10 @@ namespace PtouchPrintSender
             Console.WriteLine($"{timestamp} [INFO] {line}");
         }
 
-        //public static void Error(string format, params object[] args)
-        //{
-        //    var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-        //    var line = string.Format(format, args);
-        //    Console.WriteLine($"{timestamp} [ERROR] {line}");
-        //}
-
+        /// <summary>
+        /// ロギング(ERROR)
+        /// </summary>
+        /// <param name="args"></param>
         public static void Error(params object[] args)
         {
             var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
